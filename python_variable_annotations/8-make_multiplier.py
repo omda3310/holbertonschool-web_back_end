c8b90053981e4bdb960f88d1c3a7544ce225ab93
omda3310 <6197@holbertonstudents.com>
2023-09-24 12:03:39 +0200

    Return a function that multiplies a float by multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-annotated function make_multiplier that takes a float multiplier as argument and returns
+annotated function make_multiplier that takes
+a float multiplier as argument and returns
 a function that multiplies a float by multiplier
 """
 from typing import Callable
