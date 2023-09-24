bc9e95d0193ed29dde592d55d5188d8971371625
omda3310 <6197@holbertonstudents.com>
2023-09-24 11:16:28 +0200

    Returns their sum as a float

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Annotated function sum_mixed_list"""
+
+
+from typing import List, Union
+mx_list = Union[int, float]
+
+
+def sum_mixed_list(mxd_lst: List[mx_list]) -> List[float]:
+    """ Returns their sum as a float"""
+    return sum(mxd_lst)
