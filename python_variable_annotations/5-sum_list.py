d3b298799e3f14d4535a2f143181c05d8d7891ee
omda3310 <6197@holbertonstudents.com>
2023-09-24 10:58:45 +0200

    returns their sum as a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""Annotated function sum_list"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns sum as a float."""
+    return sum(input_list)
