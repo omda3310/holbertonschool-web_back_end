c2718216f8c8eb2e868e723d2f56cf58ede93455
omda3310 <6197@holbertonstudents.com>
2023-09-26 12:21:40 +0200

    Measure the runtime as float

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function with integers n and max_delay as arguments
+that measures the total execution time
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """Measure the runtime as float"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    return (end_time - start_time) / 2
