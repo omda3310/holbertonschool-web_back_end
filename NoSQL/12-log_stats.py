83f6c800e0e4629fd9f01f5373429340776ddb56
omda3310 <6197@holbertonstudents.com>
2023-10-02 00:08:15 +0200

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,13 +8,13 @@ if __name__ == __main__:
     """Connect to database"""
     clt = MongoClient("mongodb://127.0.0.1:27017")
     collection = clt.logs.nginx
-    number_logs = collection.count()
+    number_logs = collection.count_documents()
     print("{} logs".format(number_logs))
     print("Methods:")
     for method in Methods:
-        number_logs = collection.count("methods": method)
+        number_logs = collection.count_documents("methods": method)
         print("\tmethod {}: {}".format(method, number_logs))
-    cheked_status = collection.count("method": "GET", "path": "/status")
+    cheked_status = collection.count_documents("method": "GET", "path": "/status")
     print("{ status check}".format(cheked_status))
 
     
