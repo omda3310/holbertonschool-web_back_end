5d844920ac61ecb8acb0dad8bc9bdbd7c1446ead
omda3310 <6197@holbertonstudents.com>
2023-10-02 00:06:31 +0200

    Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""A Python script that provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+Methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+if __name__ == __main__:
+    """Connect to database"""
+    clt = MongoClient("mongodb://127.0.0.1:27017")
+    collection = clt.logs.nginx
+    number_logs = collection.count()
+    print("{} logs".format(number_logs))
+    print("Methods:")
+    for method in Methods:
+        number_logs = collection.count("methods": method)
+        print("\tmethod {}: {}".format(method, number_logs))
+    cheked_status = collection.count("method": "GET", "path": "/status")
+    print("{ status check}".format(cheked_status))
+
+    
+
+
+
