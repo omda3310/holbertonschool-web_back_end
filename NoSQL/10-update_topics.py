b31290adb0e99ed2cfc1740ac918b83f0ce56cec
omda3310 <6197@holbertonstudents.com>
2023-10-01 14:59:05 +0200

    Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A Python function that changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update all topics"""
+    mongo_collection.Update_many({"name": name}, {"$set" {"topics": topics}})
