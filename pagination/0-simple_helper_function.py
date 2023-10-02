7c8462583777665fda3bf5638a36746cee80e04f
omda3310 <6197@holbertonstudents.com>
2023-10-02 14:40:14 +0200

    Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-a function named index_range that takes two integer arguments page and page_size
+a function named index_range that takes two
+integer arguments page and page_size
 """
 
 from typing import Tuple
