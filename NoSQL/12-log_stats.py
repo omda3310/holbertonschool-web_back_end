56da832a3d8e74d65b22c6e3e8c958018e7447b3
omda3310 <6197@holbertonstudents.com>
2023-10-02 11:14:29 +0200

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,20 +4,17 @@ from pymongo import MongoClient
 Methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-def status_log(nginx_collection):
+if __name__ == '__main__':
+    """Connect to database"""
+    clt = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = clt.logs.nginx
     """a Python script that provides some stats about Nginx logs stored in MongoDB"""
     number_logs = nginx_collection.count_documents({})
     print(f'{number_logs} logs')
     print("Methods:")
     for method in Methods:
-        number_logs = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {number_logs}')
+        number_logs_method = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {number_logs_method}')
+
     cheked_status = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f'{cheked_status} status check')
-    
-    
-if __name__ == "__main__":
-    """Connect to database"""
-    clt = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = clt.logs.nginx
-    status_log(nginx_collection)
+    print(f'{cheked_status} status check') 
