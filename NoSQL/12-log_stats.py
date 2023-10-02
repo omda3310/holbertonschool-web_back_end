babfe2880ab459cbadc0825a9138d60ed59edd1a
omda3310 <6197@holbertonstudents.com>
2023-10-02 10:52:55 +0200

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,16 +8,16 @@ def status_log(n_collection, var=None):
     """a Python script that provides some stats about Nginx logs stored in MongoDB"""
     if var:
         number_logs = n_collection.count_documents({"method": var})
-        print("\tmethod {}: {}".format(var, number_logs))
+        print(f'\tmethod {var}: {number_logs}')
         return
     number_logs = n_collection.count_documents({})
-    print("{} logs".format(number_logs))
+    print(f'{number_logs} logs')
     print("Methods:")
     for method in Methods:
         number_logs = n_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, number_logs))
+        print(f'\tmethod {method}: {number_logs}')
     cheked_status = n_collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(cheked_status))
+    print(f'{cheked_status} status check')
     
     
     if __name__ == "__main__":
