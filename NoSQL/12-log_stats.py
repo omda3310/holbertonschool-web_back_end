deb4d6fc9aa12e3234df55493b3e8aad33b266cb
omda3310 <6197@holbertonstudents.com>
2023-10-02 11:41:09 +0200

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-"""A Python script that provides some stats about Nginx logs stored in MongoDB"""
+"""A Python script that provides some stats about Nginx logs stored in MongoDB:"""
 from pymongo import MongoClient
 
 
 if __name__ == '__main__':
-    """Connect to database"""
-    clt = MongoClient()
-    nginx_collection = clt.logs.nginx
-    """a Python script that provides some stats about Nginx logs stored in MongoDB"""
-    number_logs = nginx_collection.count_documents({})
-    print(f'{number_logs} logs')
+    client = MongoClient()
+    db = client.logs
+    nginx_collection = db.nginx
 
-    Methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    number_logs = nginx_collection.count_documents({})
+    print(f"{number_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in Methods:
         number_logs_method = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {number_logs_method}')
+        print(f"\tmethod {method}: {number_logs_method}")
 
-    cheked_status = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f'{cheked_status} status check') 
+    cheked_status = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{cheked_status} status check")
+
