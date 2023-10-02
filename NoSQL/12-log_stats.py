d5e8a872d39f5fef8d4f2fd403e402658363ae36
omda3310 <6197@holbertonstudents.com>
2023-10-02 11:01:37 +0200

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,24 +4,24 @@ from pymongo import MongoClient
 Methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-def status_log(n_collection, var=None):
+def status_log(nginx_collection, var=None):
     """a Python script that provides some stats about Nginx logs stored in MongoDB"""
     if var:
-        number_logs = n_collection.count_documents({"method": var})
+        number_logs = nginx_collection.count_documents({"method": var})
         print(f'\tmethod {var}: {number_logs}')
         return
-    number_logs = n_collection.count_documents({})
+    number_logs = nginx_collection.count_documents({})
     print(f'{number_logs} logs')
     print("Methods:")
     for method in Methods:
-        number_logs = n_collection.count_documents({"method": method})
+        number_logs = nginx_collection.count_documents({"method": method})
         print(f'\tmethod {method}: {number_logs}')
-    cheked_status = n_collection.count_documents({"method": "GET", "path": "/status"})
+    cheked_status = nginx_collection.count_documents({"method": "GET", "path": "/status"})
     print(f'{cheked_status} status check')
     
     
     if __name__ == "__main__":
         """Connect to database"""
         clt = MongoClient('mongodb://127.0.0.1:27017')
-        n_collection = clt.logs.nginx
-        status_log(n_collection)
+        nginx_collection = clt.logs.nginx
+        status_log(nginx_collection)
