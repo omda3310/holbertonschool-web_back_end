235cc5f5786ba93f55d610b0d6f335a22d2bc8c2
omda3310 <6197@holbertonstudents.com>
2023-10-11 12:21:29 +0200

    ES6 method properties

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,5 +1,6 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
+
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
