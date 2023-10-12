09f06b91b48faf8b04927c69215abe54b25822fb
omda3310 <6197@holbertonstudents.com>
2023-10-12 17:39:09 +0200

    Implement a class named Car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+export class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.construtor();
+  }
+}
