74d739e0093b9015cc9375cc30ed8965aa9549f6
omda3310 <6197@holbertonstudents.com>
2023-12-01 12:17:27 +0100

    Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let ms = '';
+  let msg = '';
   return Promise.all([
     uploadPhoto(),
     createUser(),
   ]).then((data) => {
-    ms += `${data[0].body} ${data[1].firstName} ${data[2].lastName}`;
+    msg += `${data[0].body} ${data[1].firstName} ${data[2].lastName}`;
     console.log(ms);
   }).catch(() => {
     console.log('Signup system offline');
