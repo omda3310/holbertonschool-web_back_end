cd9d3fa7dfe4c7bb49df017ccda9e3a58be4c314
omda3310 <6197@holbertonstudents.com>
2023-12-01 12:15:26 +0100

    Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -9,6 +9,6 @@ export default function handleProfileSignup() {
     ms += `${data[0].body} ${data[1].firstName} ${data[2].lastName}`;
     console.log(ms);
   }).catch(() => {
-    console.log('body firstName lastName');
+    console.log('Signup system offline');
   });
 }
