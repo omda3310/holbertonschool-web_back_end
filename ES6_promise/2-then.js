8a0f1c79de709e5581114a8bb7adc2247b2cb8a5
omda3310 <6197@holbertonstudents.com>
2023-12-01 11:45:54 +0100

    Catch me if you can!

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+export default function handleResponseFromAPI(promise) {
+  return Promise.then(() => ({ status: 200, body: 'success'}))
+  .catch(() => Error())
+  .finally(() => {
+    console.log('Got a response from the API')
+  })
+}
