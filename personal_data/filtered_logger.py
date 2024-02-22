e95de33afbf9681c303dc8e6b53b657bff39993a
omda3310 <6197@holbertonstudents.com>
2024-02-22 12:17:58 +0100

    message obfuscated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,15 +7,16 @@ import os
 import re
 from typing import List
 
-def filter_datum (fields: List[str], redaction: str, message: str, separator: str) -> str:
-  """Arguments:
-fields: a list of strings representing all fields to obfuscate
-redaction: a string representing by what the field will be obfuscated
-message: a string representing the log line
-separator: a string representing by which character is separating all fields in the log line (message)
-"""
 
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+  """Arguments:
+  fields: a list of strings representing all fields to obfuscate
+  redaction: a string representing by what the field will be obfuscated
+  message: a string representing the log line
+  separator: a string representing by which character is separating
+  all fields in the log line (message)
+  """
   for f in fields:
     message = re.sub(rf"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
   return message
-  
