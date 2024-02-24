c7f58273911bfc932b5d737ae05189d9eb373970
omda3310 <6197@holbertonstudents.com>
2024-02-24 13:54:00 +0100

    Check valid password

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python3
+"""Encrypting passwords"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Expect one string argument and returns a byte string.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    validate that the provided password matches the hashed password
+    """"
+    return bcrypt.checkpw(password.encode('utf-8'), hash_password)
