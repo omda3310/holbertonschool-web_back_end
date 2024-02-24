e84341d8e18d35ba36e7bfed52e9cdd7699daac5
omda3310 <6197@holbertonstudents.com>
2024-02-24 12:32:07 +0100

    Connect to secure database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -70,8 +70,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """
 
     connx = mysql.connector.connect(
-        username=os.getenv("PERSONAL_DATA_DB_USERNAME ", "root"),
-        password=os.getenv("PERSONAL_DATA_DB_PASSWORD ", " "),
+        username=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
         host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
         database=os.getenv("PERSONAL_DATA_DB_NAME")
     )
