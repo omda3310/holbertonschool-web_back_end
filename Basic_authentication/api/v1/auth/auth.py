c24162f827c4f055e5d50affc3f34ef9c258931d
omda3310 <6197@holbertonstudents.com>
2024-02-25 14:39:57 +0100

    class authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,15 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """check authentification"""
-        return False
+        if path is None or excluded_paths is None or Not len(excluded_paths):
+            return True
+        if path[-1] != '\':
+            path += '\'
+        if excluded_paths[-1] != '\':
+            excluded_paths += '\'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """handle authorisation"""
