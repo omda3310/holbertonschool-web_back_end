b53898c407b0405b7d032a3104004de85fa782d6
omda3310 <6197@holbertonstudents.com>
2024-02-29 11:10:21 +0100

    add new line

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -21,6 +21,7 @@ elif AUTH_TYPE = "basic_auth":
     from api.v1.auth.basic_auth import basic_auth
     auth = basic_auth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -48,8 +49,8 @@ def before_request() -> str:
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/',
-             '/api/v1/unauthorized/',
-             '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
