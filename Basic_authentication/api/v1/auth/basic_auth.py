0a8f85bab12d4c0ad44bdd106e857857608eb2d3
omda3310 <6197@holbertonstudents.com>
2024-03-03 21:33:18 +0100

    User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,19 +42,18 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-        def extract_user_credentials(self,
-                                     decoded_base64_authorization_header:
-                                         str) -> (str, str):
-            """
-             returns the user email and password from
-             the Base64 decoded value.
-            """
-            if decoded_base64_authorization_header is None:
-                return None, None
-            if not isinstance(decoded_base64_authorization_header, str):
-                return None, None
-            if "":" not in decoded_base64_authorization_header:
-                return None, None
-            contain1 = decoded_base64_authorization_header.split(':', 1)[0]
-            contain2 = decoded_base64_authorization_header.split(':', 1)[1]
-            return contain1, contain2
+        def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+        ) -> {str, str}:
+        """Returns the user email and password from the Base64.
+        """
+        if decoded_base64_authorization_header is None \
+           or not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        contain1, contain2 = \
+            decoded_base64_authorization_header.split(':', 1)
+        return contain1, contain2
