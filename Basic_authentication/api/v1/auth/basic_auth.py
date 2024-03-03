e6cc0cc04798cbb04aff68d94087d4a574fa5b54
omda3310 <6197@holbertonstudents.com>
2024-03-03 20:24:22 +0100

    Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,3 +24,20 @@ class BasicAuth(Auth):
         enc = authorization_header.split(' ', 1)[1]
 
         return enc
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Return the decoded value of a Base64.
+        """
+        if base64_authorization_header is None \
+           or not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            code_base64 = base64_authorization_header.encode('utf-8')
+            code_message = base64.b64decode(base64_bytes)
+            message = code_message.decode('utf-8')
+            return message
+        except Exception:
+            return None
