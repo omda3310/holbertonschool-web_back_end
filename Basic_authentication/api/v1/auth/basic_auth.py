a6f1718b612be55a3826f31d398d5295682957e6
omda3310 <6197@holbertonstudents.com>
2024-03-03 22:08:52 +0100

    User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,15 +60,18 @@ class BasicAuth(Auth):
         return contain1, contain2
 
     def user_object_from_credentials(self,
-                                     user_email: str, user_pwd: str
-                                     ) -> TypeVar('User'):
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
         """
-        returns the User instance based on his email and password.
+        Returns the User instance based on his
+        email and password
         """
         if user_email is None or not isinstance(user_email, str):
             return None
+
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
+
         try:
             users = User.search({'email': user_email})
         except Exception:
@@ -77,4 +80,5 @@ class BasicAuth(Auth):
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
+
         return None
