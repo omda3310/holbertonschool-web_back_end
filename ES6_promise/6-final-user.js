87967e0bf2ee99fe702d9192595e14580ffbf0af
omda3310 <6197@holbertonstudents.com>
2024-03-23 15:10:59 +0100

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default handleProfileSignup(firstName, lastName, fileName) {
+  const singUser = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const upPhoto = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+
+  return [singUser, upPhoto];
+}
