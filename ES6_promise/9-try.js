99a1cdf3e5d6c1038a0579b60c997e73c0fc4ad2
omda3310 <6197@holbertonstudents.com>
2024-03-23 15:42:18 +0100

    try and catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push(`Error: ${errmessage}`);
+    queue.push(`Error: ${err.message}`);
   }
   queue.push('Guardrail was processed');
   return queue;
