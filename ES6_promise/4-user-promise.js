7cfb5037e7e8ad5c17b7dab9be11ac303a2df41c
omda3310 <6197@holbertonstudents.com>
2024-03-23 14:30:55 +0100

    resolved promise with this objec

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-        firstName: firstName, lastName: lastName
-      });
+  return Promise.resolve({
+    firstName, lastName,
+  });
 }
