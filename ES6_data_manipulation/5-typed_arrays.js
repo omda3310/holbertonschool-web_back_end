14099323e422768ce26db74df671772ad813990e
omda3310 <6197@holbertonstudents.com>
2024-03-24 15:56:17 +0100

    Typed Arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(lenght, position, value) {
-	const buf = new ArrayBuffer(lenght);
-	const view = new DataView(buf);
-	if (position > lenght - 1) {
-		throw Error('Position outside range');
-	}
-    view.setInt8(position, value);
-	return view;
+  const buf = new ArrayBuffer(lenght);
+  const view = new DataView(buf);
+  if (position > lenght - 1) {
+	throw Error('Position outside range');
+  }
+  view.setInt8(position, value);
+  return view;
 }
