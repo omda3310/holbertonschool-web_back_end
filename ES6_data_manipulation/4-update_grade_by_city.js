23f1389ce0e16765fe9c426c62313e0162195818
omda3310 <6197@holbertonstudents.com>
2024-03-24 14:18:14 +0100

    using map and filter

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,23 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const SG = students.filter((student) => student.location === city);
+
+  const SGed = SG.map((student) => {
+    const GFilter = newGrades.filter(
+	  (newGrade) => newGrade.studentId === student.id);
+
+      let grade;
+  
+      if (GFilter[0]) {
+        grade = GFilter[0].grade;
+      } else {
+        grade = 'N/A';
+      }
+  
+      return {
+        ...student,
+        grade,
+      };
+  });
+
+  return SGed;
+}
