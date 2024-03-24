faaa7a36c685950fa3f83b2d38438f27272afd4e
omda3310 <6197@holbertonstudents.com>
2024-03-24 13:36:31 +0100

    Display locations

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(students, city) {
   if (Array.isArray(students)) {
-    return students.filter((s) => s.location === city)
+    return students.filter((e) => e.location === city)
   }
   return [];
 }
