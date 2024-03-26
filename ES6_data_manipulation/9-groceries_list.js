a238d98ca25f0277d871cbca9e1d975aea6dbd53
omda3310 <6197@holbertonstudents.com>
2024-03-26 14:59:43 +0100

    Map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const groceries_list = new Map();
+  groceries_list.set('Apples', 10);
+  groceries_list.set('Tomatoes', 10);
+  groceries_list.set('Pasta', 1);
+  groceries_list.set('Rice', 1);
+  groceries_list.set('Banana', 5);
+
+  return groceries_list;
+}
