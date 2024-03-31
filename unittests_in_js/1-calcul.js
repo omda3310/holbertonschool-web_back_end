112151b2b5328e7f98f8044d095f8c177f2ea570
omda3310 <6197@holbertonstudents.com>
2024-04-01 00:57:06 +0200

    type funtion

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+const calculateNumber = (type, a, b) => {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+
+    switch (type) {
+        case 'SUM':
+            return roundedA + roundedB;
+        case 'SUBTRACT':
+            return roundedA - roundedB;
+        case 'DIVIDE':
+            if (roundedB === 0) {
+                return 'Error';
+            } else {
+                return roundedA / roundedB;
+            }
+        default:
+            return 0;
+    }
+}
+
+module.exports = calculateNumber;
