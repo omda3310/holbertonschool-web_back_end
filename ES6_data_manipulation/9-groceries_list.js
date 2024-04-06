0a67f84b84cc2c63b4459c0afbedc0520fc868aa
omda3310 <6197@holbertonstudents.com>
2024-04-06 16:55:19 +0200

    update

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,11 @@
 export default function groceriesList() {
-	const groceries_list = new Map();
+  const groceries = new Map();
 
-	groceries_list.set('Apples', 10);
-	groceries_list.set('Tomatoes', 10);
-	groceries_list.set('Pasta', 1);
-	groceries_list.set('Rice', 1);
-	groceries_list.set('Banana', 5);
-	return groceries_list;
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+
+  return groceries;
 }
