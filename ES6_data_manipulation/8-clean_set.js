17127432cb354e120bdd2a307bc873ec1015d42e
omda3310 <6197@holbertonstudents.com>
2024-04-06 16:41:22 +0200

    add condition

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (typeof set !== Object || startString === '' ||
-   typeof startString !== 'string') {
-    return '';
-  }
+  if (startString === '' || typeof startString !== 'string') return '';
   const lst = [];
-  set.forEach(element => {
+  set.forEach((element) => {
     if (element && element.startsWith(startString)) {
       lst.push(element.replace(startString, ''));
-    }    
+    }
   });
   return lst.join('-');
 }
