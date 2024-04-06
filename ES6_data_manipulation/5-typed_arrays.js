f40e073e601919c1a8b95bfe9a31632bc80696a0
omda3310 <6197@holbertonstudents.com>
2024-04-06 16:04:33 +0200

    fix type array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(lenght, position, value) {
   const buf = new ArrayBuffer(lenght);
-  const view = new DataView(buf, 0);
+  const view = new Int8Array(buf, 0);
   if (position > lenght - 1) {
     throw Error('Position outside range');
   }
-  view.setInt8(position, value);
+  view.set(position, value);
   return view;
 }
